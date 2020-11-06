4a56027346db3c512b72d2eea19473d859657e13
YusufArda27 <y.ardavardar@gmail.com>
2020-11-06 15:59:46 +0300

    Changed Font and Size

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "92f9381c090ad3ff4f59";
+/******/ 	var hotCurrentHash = "0cc11423e3059e222edf";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
